4a0a604e3b6271a0c7d2439c1fcfebfb0f65e236
ddos5 <yoiu2577@gmail.com>
2026-02-20 01:41:03 +0100

    Changed logic for Warrior class

--- src/main/java/org/jikvict/tasks/exposed/Warrior.java
+++ src/main/java/org/jikvict/tasks/exposed/Warrior.java
@@ -1,4 +1,32 @@
 package org.jikvict.tasks.exposed;
 
-public class Warrior {
+public class Warrior extends GameCharacter{
+    private int armorRating;
+    public Warrior(String name, int health, int baseAttackPower, int armorRating){
+        super(name, health, baseAttackPower);
+        this.armorRating = armorRating;
+    }
+    @Override
+    public String getCharacterType(){
+        return "Warrior";
+    }
+    @Override
+    public int calculateDamage(){
+        return (baseAttackPower + armorRating) / 2;
+    }
+    @Override
+    public void takeDamage(int damage){
+        super.takeDamage(Math.max(0, damage - armorRating / 3));
+    }
+    @Override
+    public String getInfo(){
+        return super.getInfo() + "Armor: " + armorRating;
+    }
+
+    public int getArmorRating() {
+        return armorRating;
+    }
+    public void setArmorRating(int armorRating){
+        this.armorRating = armorRating;
+    }
 }
