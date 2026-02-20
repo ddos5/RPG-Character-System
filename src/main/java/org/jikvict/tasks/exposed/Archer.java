5f428fc364a705ae994147cdd82481ac8f02eebb
ddos5 <yoiu2577@gmail.com>
2026-02-20 01:44:22 +0100

    Changed logic for Archer class

--- src/main/java/org/jikvict/tasks/exposed/Archer.java
+++ src/main/java/org/jikvict/tasks/exposed/Archer.java
@@ -1,4 +1,36 @@
 package org.jikvict.tasks.exposed;
 
-public class Archer {
+public class Archer extends GameCharacter{
+    private int arrowCount;
+    public Archer(String name, int health, int baseAttackPower, int arrowCount){
+        super(name, health, baseAttackPower);
+        this.arrowCount = arrowCount;
+    }
+    @Override
+    public String getCharacterType(){
+        return "Archer";
+    }
+    @Override
+    public int calculateDamage(){
+        return baseAttackPower * 5;
+    }
+    @Override
+    public String getInfo(){
+        return super.getInfo() + " | Arrows: " + arrowCount;
+    }
+    public boolean shootArrow(){
+        if(arrowCount > 0){
+            arrowCount--;
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    public int getArrowCount(){
+        return arrowCount;
+    }
+    public void setArrowCount(int arrowCount){
+        this.arrowCount = arrowCount;
+    }
 }
