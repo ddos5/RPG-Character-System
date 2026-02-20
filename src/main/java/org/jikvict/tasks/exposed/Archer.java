ce9a99fb278d65bb7c47dec9f0e51870375abe37
ddos5 <yoiu2577@gmail.com>
2026-02-20 02:11:27 +0100

    Archer damage bug fixed (*5 to +5)

--- src/main/java/org/jikvict/tasks/exposed/Archer.java
+++ src/main/java/org/jikvict/tasks/exposed/Archer.java
@@ -12,7 +12,7 @@ public class Archer extends GameCharacter{
     }
     @Override
     public int calculateDamage(){
-        return baseAttackPower * 5;
+        return baseAttackPower + 5;
     }
     @Override
     public String getInfo(){
