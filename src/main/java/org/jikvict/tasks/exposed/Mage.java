c582932d2142efc22d4093cab0006a294ceb70f2
ddos5 <yoiu2577@gmail.com>
2026-02-20 01:43:25 +0100

    Changed logic for Mage class and added some changes with getters

--- src/main/java/org/jikvict/tasks/exposed/Mage.java
+++ src/main/java/org/jikvict/tasks/exposed/Mage.java
@@ -1,4 +1,65 @@
 package org.jikvict.tasks.exposed;
 
-public class Mage {
+public class Mage extends GameCharacter{
+    private int mana;
+    private final int maxMana;
+    public Mage(String name, int health, int basePowerAttack, int mana){
+        super(name, health, basePowerAttack);
+        this.mana = mana;
+        this.maxMana = mana;
+
+    }
+    @Override
+    public String getCharacterType(){
+        return "Mage";
+    }
+    @Override
+    public int calculateDamage(){
+        return baseAttackPower * 2;
+    }
+    @Override
+    public String getInfo(){
+        return super.getInfo() + " | Mana: " + mana + "/" + maxMana;
+    }
+    public String castSpell(){
+        if(mana < 10){
+            return getName() + "does not have enough mana!";
+        }
+        else{
+            mana-=10;
+            return getName() + "casts a spell!";
+        }
+    }
+
+    public String castSpell(String spellName){
+        if(mana < 10){
+            return getName() + "does not have enough mana!";
+        }
+        else{
+            mana-=10;
+            return getName() + "casts" + spellName + "!";
+        }
+    }
+
+    public String castSpell(String spellName, GameCharacter target){
+        if(mana < 10){
+            return getName() + "does not have enough mana!";
+        }
+        else{
+            mana-=10;
+            int damage = calculateDamage();
+            target.takeDamage(damage);
+            return getName() + "casts" + spellName + "on" + target.getName() + "!";
+        }
+    }
+    public int getMana(){
+        return mana;
+    }
+    public int getMaxMana(){
+        return maxMana;
+    }
+    public void setMana(int mana){
+        this.mana = mana;
+    }
+
 }
